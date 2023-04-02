f6c4cb0e7003beddcb519c467acac9deda258ad0
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-02 16:51:18 +0300

    025

--- modelhandler.cpp
+++ modelhandler.cpp
@@ -1,2 +1,16 @@
+#include <QJsonDocument>
+#include <QJsonArray>
+#include <QJsonObject>
+
 #include "modelhandler.hpp"
 
+void DataModelHandler::get(const QString &additionalPath)
+{
+    QNetworkReply *reply = sendCRUDRequest(additionalPath, {}, "GET");
+    replyHandler(reply, "Get request successfully!");
+
+    QJsonDocument jsonResponse = QJsonDocument::fromJson(QString(reply->readAll()).toUtf8());
+
+    QJsonArray array = jsonResponse.array();
+    parseJsonArray(array);
+}
