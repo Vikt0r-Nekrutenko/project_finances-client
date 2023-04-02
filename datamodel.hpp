2dcfe85aa621c1fa60a1a98528671f56229afb45
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-02 16:04:30 +0300

    011

--- datamodel.hpp
+++ datamodel.hpp
@@ -16,10 +16,13 @@ const QMap<const RequestType, const QString> Requests
     { RequestType::DELETE,  "DELETE" },
 };
 
+const QString MainPath = "http://127.0.0.1:8000/api/";
+
 class DataModel
 {
 public:
     virtual QNetworkReply *sendCRUDRequest(const QString &addtionalPath, const QJsonObject &data, const QString &request);
+    void replyHandler(QNetworkReply *reply, const QString &noErrorMsg) const;
 };
 
 #endif // DATAMODEL_HPP
