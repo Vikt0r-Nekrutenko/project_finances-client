f4b5e4442cc0fbc5acc8bc85f01c2a66b38ca1e4
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-02 22:02:13 +0300

    054

--- operationmodelhandler.cpp
+++ operationmodelhandler.cpp
@@ -1,6 +1,11 @@
 #include "operationmodelhandler.hpp"
 
 
+OperationModelHandler::OperationModelHandler()
+{
+    get("operations/");
+}
+
 const QVector<OperationModel> &OperationModelHandler::operations() const
 {
     return mOperations;
