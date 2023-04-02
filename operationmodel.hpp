d20a36392b74934b2c128ea6857965dec25801b6
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-02 21:44:07 +0300

    045

--- operationmodel.hpp
+++ operationmodel.hpp
@@ -8,6 +8,12 @@ class OperationModel : public CRUDModel
 public:
     OperationModel(int id, const QString &date, const QString &deposit, int amount, const QString &category);
 
+    void create() override;
+    void read() override;
+    void update() override;
+    void remove() override;
+    void parseJsonObject(const QJsonObject &) override;
+
 private:
 
     QString mDate;
