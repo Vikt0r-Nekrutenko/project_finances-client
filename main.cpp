43ce8e082ffa48373b9aa71c6c14b334ffcc4fdb
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-02 15:30:52 +0300

    003

--- main.cpp
+++ main.cpp
@@ -1,23 +1,73 @@
 #include <QCoreApplication>
 #include <window.hpp>
+#include <smv/iview.hpp>
+
+class CloseView : public stf::smv::IView
+{
+public:
+    CloseView(stf::smv::BaseModel *model)
+        : stf::smv::IView(model) { }
+
+    bool isContinue() const override
+    {
+        return false;
+    }
+
+    void show(stf::Renderer&) override { };
+
+    IView* update(const float) override
+    {
+        return this;
+    }
+};
+
+class MenuView : public stf::smv::IView
+{
+public:
+    MenuView(stf::smv::BaseModel *model)
+        : stf::smv::IView(model)
+    {
+
+    }
+    void show(stf::Renderer &renderer) override
+    {
+
+    }
+
+    stf::smv::IView *keyEventsHandler(const int key) override
+    {
+        switch (key) {
+        case 'q':
+            return new CloseView(nullptr);
+            break;
+        default:
+            break;
+        }
+        return this;
+    }
+};
 
 class MainWindow : public stf::Window
 {
-public:
+    MenuView menuView = MenuView(nullptr);
+    CloseView closeView = CloseView(nullptr);
+    stf::smv::IView *currentView = &menuView;
 
+public:
     bool onUpdate(const float dt) override
     {
-        return true;
+        currentView->show(renderer);
+        return currentView->isContinue();
     }
 
     void keyEvents(const int key) override
     {
-
+        currentView->keyEventsHandler(key);
     }
 
     void mouseEvents(const stf::MouseRecord &mr) override
     {
-
+        currentView->mouseEventsHandler(mr);
     }
 };
 
