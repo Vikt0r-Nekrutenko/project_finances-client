7e6fcad0186fb6013736e53134a6540759caeb7c
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-03 00:12:33 +0300

    084

--- appmodel.cpp
+++ appmodel.cpp
@@ -24,7 +24,7 @@ void AppModel::addNewDeposit(const char *name, int balance)
 
 void AppModel::changeBalance(int index, int balance)
 {
-    if(index >= mDepositHandler.deposits().size())
+    if(index >= mDepositHandler.deposits().size() || index < 0)
         throw std::out_of_range("Deposit with that index does not exitst.");
     mDepositHandler.updateBalance(index, balance);
 }
@@ -44,9 +44,16 @@ void AppModel::addNewOperation(const char *date, const char *deposit, int amount
     mOperationHandler.addNewOperation(date, deposit, amount, category);
 }
 
+void AppModel::changeOperation(int index, const char *date, const char *deposit, int amount, const char *category)
+{
+    if(index >= mOperationHandler.operations().size() || index < 0)
+        throw std::out_of_range("Operation with that index does not exitst.");
+    mOperationHandler.updateOperation(index, date, deposit, amount, category);
+}
+
 void AppModel::deleteOperation(int index)
 {
     if(index >= mOperationHandler.operations().size() || index < 0)
-        throw std::out_of_range("Deposit with that index does not exitst.");
+        throw std::out_of_range("Operation with that index does not exitst.");
     mOperationHandler.deleteOperation(index);
 }
