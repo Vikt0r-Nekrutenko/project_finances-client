6ccfd253a021105eec0954c33fa1a3cf22531679
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-02 22:04:46 +0300

    056

--- operationmodelhandler.cpp
+++ operationmodelhandler.cpp
@@ -6,6 +6,26 @@ OperationModelHandler::OperationModelHandler()
     get("operations/");
 }
 
+void OperationModelHandler::addNewOperation(const QString &date, const QString &deposit, int amount, const QString &category)
+{
+
+}
+
+void OperationModelHandler::updateOperation(int index, const QString &date, const QString &deposit, int amount, const QString &category)
+{
+
+}
+
+void OperationModelHandler::deleteOperation(int index)
+{
+
+}
+
+void OperationModelHandler::parseJsonArray(const QJsonArray &replyJsonArray)
+{
+
+}
+
 const QVector<OperationModel> &OperationModelHandler::operations() const
 {
     return mOperations;
