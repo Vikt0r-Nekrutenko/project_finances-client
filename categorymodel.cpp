99fcdb33feefce045c51d7460d1af2795cef2068
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-03 00:33:09 +0300

    088

--- categorymodel.cpp
+++ categorymodel.cpp
@@ -1,6 +1,14 @@
 #include "categorymodel.hpp"
 
 CategoryModel::CategoryModel(const QString &name, const QString &type)
-{
+    : mName{name}, mType{type} {}
 
+const QString &CategoryModel::name() const
+{
+    return mName;
+}
+
+const QString &CategoryModel::type() const
+{
+    return mType;
 }
