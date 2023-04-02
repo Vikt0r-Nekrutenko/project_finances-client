1d3b05f675fb5059cb865448bc5650838eefa426
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-02 21:46:08 +0300

    047

--- operationmodel.cpp
+++ operationmodel.cpp
@@ -1,4 +1,21 @@
+#include <QJsonObject>
+#include <QJsonDocument>
+
 #include "operationmodel.hpp"
 
 OperationModel::OperationModel(int id, const QString &date, const QString &deposit, int amount, const QString &category)
     : mDate{date}, mDeposit{deposit}, mCategory{category}, mId{id}, mAmount{amount} {}
+
+void OperationModel::create()
+{
+    QJsonObject newOperation {
+        {"id", mId },
+        {"date", mDate },
+        {"deposit", mDeposit },
+        {"amount", mAmount },
+        {"category", mCategory },
+    };
+
+    QNetworkReply *reply = sendCRUDRequest("operations/", newOperation, "POST");
+    replyHandler(reply, "Operation added successfully!");
+}
