8e63a2c695635c25cb341ceeae330c6ed3a69199
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-02 21:44:57 +0300

    046

--- operationmodel.cpp
+++ operationmodel.cpp
@@ -1,2 +1,4 @@
 #include "operationmodel.hpp"
 
+OperationModel::OperationModel(int id, const QString &date, const QString &deposit, int amount, const QString &category)
+    : mDate{date}, mDeposit{deposit}, mCategory{category}, mId{id}, mAmount{amount} {}
