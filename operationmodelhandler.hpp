2d4fbaa4736aa51b2a4627591935ad13a9534c64
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-02 22:03:36 +0300

    055

--- operationmodelhandler.hpp
+++ operationmodelhandler.hpp
@@ -9,6 +9,11 @@ class OperationModelHandler : public DataModelHandler
 public:
     OperationModelHandler();
 
+    void addNewOperation(const QString &date, const QString &deposit, int amount, const QString &category);
+    void updateOperation(int index, const QString &date, const QString &deposit, int amount, const QString &category);
+    void deleteOperation(int index);
+    void parseJsonArray(const QJsonArray &replyJsonArray) override;
+
     const QVector<OperationModel> &operations() const;
 
 private:
