11888c33eb91c6a6e3fd59e7c1ce54bd8474b1fc
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-02 22:14:44 +0300

    068

--- operationslistview.hpp
+++ operationslistview.hpp
@@ -9,6 +9,9 @@ class OperationsListView : public ModelViewWithInputField
 {
 public:
     OperationsListView(AppModel *model);
+    void show(stf::Renderer &renderer) override;
+    void onEnterHandler() override;
+    stf::smv::IView *keyEventsHandler(const int key) override;
 };
 
 #endif // OPERATIONSLISTVIEW_HPP
