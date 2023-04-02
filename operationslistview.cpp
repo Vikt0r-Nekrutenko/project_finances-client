4ddf0d34e8b1c7a1b09b990b0ba0772020cf2323
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-02 22:17:43 +0300

    072

--- operationslistview.cpp
+++ operationslistview.cpp
@@ -1,5 +1,6 @@
 #include "operationslistview.hpp"
 #include "appmodel.hpp"
+#include "menuview.hpp"
 
 OperationsListView::OperationsListView(AppModel *model)
     : ModelViewWithInputField(model) {}
@@ -16,5 +17,22 @@ void OperationsListView::onEnterHandler()
 
 stf::smv::IView *OperationsListView::keyEventsHandler(const int key)
 {
+    if(mOption == 0) {
+        switch (key) {
+        case '1':
+        case '2':
+        case '3':
+            mOption = key - '0';
+            break;
+        case 'q':
+            return new MenuView(static_cast<AppModel*>(m_model));
+        }
+    } else {
+        try {
+            inputHandler(key);
+        } catch(const std::exception &msg) {
+            stf::Renderer::log << stf::endl << msg.what();
+        }
+    }
     return this;
 }
