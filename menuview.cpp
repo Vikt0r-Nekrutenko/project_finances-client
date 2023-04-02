13c77eb497a67135333a380ecb6f375e5dbbc9d6
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-03 02:10:46 +0300

    115

--- menuview.cpp
+++ menuview.cpp
@@ -1,6 +1,7 @@
 #include "menuview.hpp"
 #include "appmodel.hpp"
 #include "closeview.hpp"
+#include "debtlistview.hpp"
 #include "depositlistview.hpp"
 #include "operationslistview.hpp"
 #include "categorylistview.hpp"
@@ -13,7 +14,8 @@ void MenuView::show(stf::Renderer &renderer)
     renderer.drawText({0, 2}, "1.Deposits.");
     renderer.drawText({0, 3}, "2.Operations.");
     renderer.drawText({0, 4}, "3.Categories.");
-    renderer.drawText({0, 5}, "4.Press 'q' to exit.");
+    renderer.drawText({0, 5}, "4.Categories.");
+    renderer.drawText({0, 6}, "5.Press 'q' to exit.");
 }
 
 stf::smv::IView *MenuView::keyEventsHandler(const int key)
@@ -22,6 +24,7 @@ stf::smv::IView *MenuView::keyEventsHandler(const int key)
     case '1': return new DepositListView(static_cast<AppModel*>(m_model));
     case '2': return new OperationsListView(static_cast<AppModel*>(m_model));
     case '3': return new CategoryListView(static_cast<AppModel*>(m_model));
+    case '4': return new DebtListView(static_cast<AppModel*>(m_model));
     case 'q': return new CloseView(static_cast<AppModel*>(m_model));
     }
     return this;
