6117ad30780d2a3a2aba8120b7287d6b4314b74c
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-02 22:16:23 +0300

    071

--- menuview.cpp
+++ menuview.cpp
@@ -2,6 +2,7 @@
 #include "appmodel.hpp"
 #include "closeview.hpp"
 #include "depositlistview.hpp"
+#include "operationslistview.hpp"
 
 MenuView::MenuView(AppModel *model)
     : stf::smv::IView(model) {}
@@ -17,6 +18,7 @@ stf::smv::IView *MenuView::keyEventsHandler(const int key)
 {
     switch (key) {
     case '1': return new DepositListView(static_cast<AppModel*>(m_model));
+    case '2': return new OperationsListView(static_cast<AppModel*>(m_model));
     case 'q': return new CloseView(static_cast<AppModel*>(m_model));
     }
     return this;
