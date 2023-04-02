233c564cd62ed18ba2c925d5646cdbbde8233a07
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-02 16:09:35 +0300

    014

--- datamodel.cpp
+++ datamodel.cpp
@@ -1,6 +1,7 @@
 #include <QEventLoop>
 #include <QJsonDocument>
 #include "datamodel.hpp"
+#include "renderer.hpp"
 
 QNetworkReply *DataModel::sendCRUDRequest(const QString &additionalPath, const QJsonObject &data, const QString &request)
 {
@@ -18,4 +19,14 @@ QNetworkReply *DataModel::sendCRUDRequest(const QString &additionalPath, const Q
     loop.exec();
 
     return reply;
+}
+
+void DataModel::replyHandler(QNetworkReply *reply, const QString &noErrorMsg) const
+{
+    if(reply->error() == QNetworkReply::NoError) {
+        stf::Renderer::log << reply->errorString().toStdString() << stf::endl;
+    } else {
+        stf::Renderer::log << noErrorMsg.toStdString() << stf::endl;
+    }
+    reply->deleteLater();
 }
