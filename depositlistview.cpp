a14c9c11cf83bfc8f83543e92207564ea49f1f7c
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-03 13:20:10 +0300

    150

--- depositlistview.cpp
+++ depositlistview.cpp
@@ -11,33 +11,33 @@ void DepositListView::show(stf::Renderer &renderer)
 {
     AppModel *app = static_cast<AppModel*>(m_model);
 
-    renderer.drawText({0,2}, "Your deposits:");
-
-    const QVector<DepositModel> &deposits = app->deposits();
-    int i = 0;
-    for(; i < deposits.size(); ++i) {
-        renderer.draw({0, 3 + i}, "%d.%s", i+1, deposits.at(i).name().toStdString().c_str());
-        renderer.draw({17, 3 + i}, "- %s.00 UAH", std::to_string(deposits.at(i).balance()).c_str());
+    if(mOption == 0) {
+        renderer.drawText({0, 2}, "Choose an option:");
+        renderer.drawText({0, 3}, "1.Add new deposit.");
+        renderer.drawText({0, 4}, "2.Delete deposit.");
+        renderer.drawText({0, 5}, "3.Change balance.");
+        renderer.drawText({0, 6}, "q.Back to menu.");
+    } else if(mOption == 1) {
+        renderer.drawText({0, 2}, "Type 'name balance' or 'q' to step back:");
+        renderer.drawText({0, 3}, ">> ");
+        renderer.drawText({3, 3}, mInput.c_str());
+    } else if(mOption == 2) {
+        renderer.drawText({0, 2}, "Type deposit id or 'q' to step back:");
+        renderer.drawText({0, 3}, ">> ");
+        renderer.drawText({3, 3}, mInput.c_str());
+    } else if(mOption == 3) {
+        renderer.drawText({0, 2}, "Type 'id balance' or 'q' to step back:");
+        renderer.drawText({0, 3}, ">> ");
+        renderer.drawText({3, 3}, mInput.c_str());
     }
 
-    if(mOption == 0) {
-        renderer.drawText({0, 3 + i + 1}, "Choose an option:");
-        renderer.drawText({0, 3 + i + 2}, "1.Add new deposit.");
-        renderer.drawText({0, 3 + i + 3}, "2.Delete deposit.");
-        renderer.drawText({0, 3 + i + 4}, "3.Change balance.");
-        renderer.drawText({0, 3 + i + 5}, "q.Back to menu.");
-    } else if(mOption == 1) {
-        renderer.drawText({0, 3 + i + 1}, "Type 'name balance' or 'q' to step back:");
-        renderer.drawText({0, 3 + i + 2}, ">> ");
-        renderer.drawText({3, 3 + i + 2}, mInput.c_str());
-    } else if(mOption == 2) {
-        renderer.drawText({0, 3 + i + 1}, "Type deposit id or 'q' to step back:");
-        renderer.drawText({0, 3 + i + 2}, ">> ");
-        renderer.drawText({3, 3 + i + 2}, mInput.c_str());
-    } else if(mOption == 3) {
-        renderer.drawText({0, 3 + i + 1}, "Type 'id balance' or 'q' to step back:");
-        renderer.drawText({0, 3 + i + 2}, ">> ");
-        renderer.drawText({3, 3 + i + 2}, mInput.c_str());
+    renderer.drawText({0,8}, "Your deposits:");
+
+    const QVector<DepositModel> &deposits = app->deposits();
+
+    for(int i = 0; i < deposits.size(); ++i) {
+        renderer.draw({0, i + 11}, "%d.%s", i+1, deposits.at(i).name().toStdString().c_str());
+        renderer.draw({17, i + 11}, "- %s.00 UAH", std::to_string(deposits.at(i).balance()).c_str());
     }
 }
 
