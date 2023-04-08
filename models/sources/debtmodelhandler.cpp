af84c54fb5d78d8d9c6fb1213079db8b024c201b
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-08 22:08:20 +0300

    237

--- models/sources/debtmodelhandler.cpp
+++ models/sources/debtmodelhandler.cpp
@@ -8,7 +8,7 @@ DebtModelHandler::DebtModelHandler()
     get("debts/");
 }
 
-void DebtModelHandler::addNewDebt(const QString &name, int amount)
+void DebtModelHandler::addNewDebt(const std::string &name, int amount)
 {
     mDebts.push_back(DebtModel(
                               mDebts.empty() ? 0 : mDebts.back().mId + 1,
@@ -17,7 +17,7 @@ void DebtModelHandler::addNewDebt(const QString &name, int amount)
     mDebts.back().create();
 }
 
-void DebtModelHandler::updateDebt(int index, const QString &name, int amount)
+void DebtModelHandler::updateDebt(int index, const std::string &name, int amount)
 {
     mDebts[index].mName = name;
     mDebts[index].mAmount = amount;
@@ -42,7 +42,7 @@ void DebtModelHandler::parseJsonArray(const QJsonArray &replyJsonArray)
     }
 }
 
-const QVector<DebtModel> &DebtModelHandler::debts() const
+const std::vector<DebtModel> &DebtModelHandler::debts() const
 {
     return mDebts;
 }
