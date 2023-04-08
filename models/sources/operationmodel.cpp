f81e66fe29eb7f4f194a711f61d466a382b4a69c
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-08 22:03:23 +0300

    233

--- models/sources/operationmodel.cpp
+++ models/sources/operationmodel.cpp
@@ -10,10 +10,10 @@ void OperationModel::create()
 {
     QJsonObject newOperation {
         {"id", mId },
-        {"date", mDate },
-        {"deposit", mDeposit },
+        {"date", mDate.c_str() },
+        {"deposit", mDeposit.c_str() },
         {"amount", mAmount },
-        {"category", mCategory },
+        {"category", mCategory.c_str() },
     };
 
     QNetworkReply *reply = sendCRUDRequest("operations/", newOperation, "POST");
@@ -22,7 +22,7 @@ void OperationModel::create()
 
 void OperationModel::read()
 {
-    QNetworkReply *reply = sendCRUDRequest("operations/" + QString::number(mId) + '/', {}, "GET");
+    QNetworkReply *reply = sendCRUDRequest("operations/" + std::to_string(mId) + '/', {}, "GET");
     replyHandler(reply, "Operation read successfully!");
 
     QJsonDocument jsonResponse = QJsonDocument::fromJson(QString(reply->readAll()).toUtf8());
@@ -34,29 +34,29 @@ void OperationModel::update()
 {
     QJsonObject selectedOperation {
         {"id", mId },
-        {"date", mDate },
-        {"deposit", mDeposit },
+        {"date", mDate.c_str() },
+        {"deposit", mDeposit.c_str() },
         {"amount", mAmount },
-        {"category", mCategory },
+        {"category", mCategory.c_str() },
     };
 
-    QNetworkReply *reply = sendCRUDRequest("operations/" + QString::number(mId) + '/', selectedOperation, "PUT");
+    QNetworkReply *reply = sendCRUDRequest("operations/" + std::to_string(mId) + '/', selectedOperation, "PUT");
     replyHandler(reply, "Operation updated successfully!");
 }
 
 void OperationModel::remove()
 {
-    QNetworkReply *reply = sendCRUDRequest("operations/" + QString::number(mId) + '/', {}, "DELETE");
+    QNetworkReply *reply = sendCRUDRequest("operations/" + std::to_string(mId) + '/', {}, "DELETE");
     replyHandler(reply, "Operation delete successfully!");
 }
 
 void OperationModel::parseJsonObject(const QJsonObject &object)
 {
     mId = object["id"].toInt();
-    mDate = object["date"].toString();
-    mDeposit = object["deposit"].toString();
+    mDate = object["date"].toString().toStdString();
+    mDeposit = object["deposit"].toString().toStdString();
     mAmount = object["amount"].toInt();
-    mCategory = object["category"].toString();
+    mCategory = object["category"].toString().toStdString();
 }
 
 int OperationModel::id() const
