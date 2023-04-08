a9e66344571e71ccdff6824c87bf93fdc46ca4c7
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-08 21:51:49 +0300

    223

--- models/sources/datamodel.cpp
+++ models/sources/datamodel.cpp
@@ -5,10 +5,10 @@
 #include "datamodel.hpp"
 #include "renderer.hpp"
 
-QNetworkReply *DataModel::sendCRUDRequest(const QString &additionalPath, const QJsonObject &data, const QString &request)
+QNetworkReply *DataModel::sendCRUDRequest(const std::string &additionalPath, const QJsonObject &data, const std::string &request)
 {
     static QNetworkAccessManager mManager;
-    QNetworkRequest mRequest {QUrl(MainPath + additionalPath)};
+    QNetworkRequest mRequest {QUrl((MainPath + additionalPath).c_str())};
 
     mRequest.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
     mRequest.setRawHeader("accept", "application/json");
@@ -20,10 +20,10 @@ QNetworkReply *DataModel::sendCRUDRequest(const QString &additionalPath, const Q
             throw std::invalid_argument("file: " + authFile.fileName().toStdString() + " doesn't opened!");
 
         QJsonObject obj = QJsonDocument::fromJson(QString(authFile.readAll()).toUtf8()).object();
-        mAuthName = obj["name"].toString();
-        mAuthValue = obj["value"].toString();
+        mAuthName = obj["name"].toString().toStdString();
+        mAuthValue = obj["value"].toString().toStdString();
     }
-    mRequest.setRawHeader(mAuthName.toUtf8(), mAuthValue.toUtf8());
+    mRequest.setRawHeader(mAuthName.c_str(), mAuthValue.c_str());
 
     QEventLoop loop;
 
@@ -35,7 +35,7 @@ QNetworkReply *DataModel::sendCRUDRequest(const QString &additionalPath, const Q
     return reply;
 }
 
-void DataModel::replyHandler(QNetworkReply *reply, const QString &noErrorMsg) const
+void DataModel::replyHandler(QNetworkReply *reply, const std::string &noErrorMsg) const
 {
     if(reply->error() == QNetworkReply::NoError) {
         stf::Renderer::log << stf::endl << noErrorMsg.toStdString();
