b64b4f10d392cbea44edac932c405a77806eb7b3
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-08 22:13:37 +0300

    240

--- models/sources/categorymodelhandler.cpp
+++ models/sources/categorymodelhandler.cpp
@@ -16,7 +16,7 @@ void CategoryModelHandler::addNewCategory(const std::string &name, const std::st
 void CategoryModelHandler::deleteCategory(int index)
 {
     mCategories[index].remove();
-    mCategories.removeAt(index);
+    mCategories.erase(mCategories.begin() + index);
 }
 
 void CategoryModelHandler::parseJsonArray(const QJsonArray &replyJsonArray)
@@ -24,8 +24,8 @@ void CategoryModelHandler::parseJsonArray(const QJsonArray &replyJsonArray)
     mCategories.clear();
     for (const auto &var : replyJsonArray) {
         mCategories.push_back(CategoryModel{
-            var.toObject()["name"].toString(),
-            var.toObject()["type"].toString()
+            var.toObject()["name"].toString().toStdString(),
+            var.toObject()["type"].toString().toStdString()
         });
     }
 }
@@ -40,7 +40,7 @@ std::vector<CategoryModel> &CategoryModelHandler::categories()
     return mCategories;
 }
 
-std::vector::iterator CategoryModelHandler::findByName(const std::string &name)
+std::vector<CategoryModel>::iterator CategoryModelHandler::findByName(const std::string &name)
 {
     return std::find_if(mCategories.begin(), mCategories.end(), [&](const CategoryModel &model){
         return model.name() == name;
