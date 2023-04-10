cc124ffc1c35811b502a91d4313f758754036db1
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-10 19:33:07 +0300

    265

--- models/sources/appmodel.cpp
+++ models/sources/appmodel.cpp
@@ -90,6 +90,7 @@ void AppModel::updateStats()
         mSumByFavCategories.push_back({favcat.name(), getSum30DaysOfOperationsByCategory(favcat)});
 
     getOperationsByCurrentMonth();
+    getMinMaxLossesBy30Days();
 }
 
 void AppModel::updateAllHandlers()
@@ -369,23 +370,25 @@ void AppModel::getMinMaxLossesBy30Days()
             operations.push_back(&operation);
     }
 
-    std::vector<std::pair<int, std::string>> operationsSum;
-    for(size_t i = 0; i < operations.size() - 1; ++i) {
+//    std::vector<std::pair<int, std::string>> operationsSum;
+    std::map<std::string, int> operationsSum;
+    for(size_t i = 0; i < operations.size(); ++i) {
         auto category = mCategoryHandler.findByName(operations.at(i)->category());
         if(category->type() == "negative") {
-            operationsSum.push_back({operations.at(i)->amount(), operations.at(i)->category()});
-            for(size_t j = i + 1; j < operations.size(); ++j) {
-                if(operations.at(i)->category() == operations.at(j)->category())
-                    operationsSum.back().first += operations.at(j)->amount();
-            }
+//            operationsSum.push_back({operations.at(i)->amount(), operations.at(i)->category()});
+            operationsSum[operations.at(i)->category()] += operations.at(i)->amount();
+//            for(size_t j = i + 1; j < operations.size(); ++j) {
+//                if(operationsSum.back().second == operations.at(j)->category())
+//                    operationsSum.back().first += operations.at(j)->amount();
+//            }
         }
     }
-    mMinMaxLosses.first = operationsSum.front(); //Min
-    mMinMaxLosses.second = operationsSum.front(); //Max
-    for(const auto& mm : operationsSum) {
-        if(mm.first < mMinMaxLosses.first.first)
-            mMinMaxLosses.first = mm;
-        else if(mm.first > mMinMaxLosses.second.first)
-            mMinMaxLosses.second = mm;
-    }
+    mMinMaxLosses.first = {operationsSum.begin()->second, operationsSum.begin()->first}; //Min
+//    mMinMaxLosses.second = operationsSum.front(); //Max
+//    for(const auto& mm : operationsSum) {
+//        if(mm.first < mMinMaxLosses.first.first)
+//            mMinMaxLosses.first = mm;
+//        if(mm.first > mMinMaxLosses.second.first)
+//            mMinMaxLosses.second = mm;
+//    }
 }
