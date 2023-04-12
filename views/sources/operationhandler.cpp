770b9845c19a76e0d4179db04f39c4b27af52d49
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-12 21:02:37 +0300

    288

--- views/sources/operationhandler.cpp
+++ views/sources/operationhandler.cpp
@@ -1,6 +1,13 @@
 #include "operationhandler.hpp"
 #include "appmodel.hpp"
 
+void OperationHandler::updateOperationsList(AppModel *model, const std::string &date)
+{
+    const int year = QDateTime().fromString(date.c_str(), "yyyy-MM-dd").date().year();
+    const int month = QDateTime().fromString(date.c_str(), "yyyy-MM-dd").date().month();
+    static_cast<AppModel*>(model)->getOperationsByMonth(year, month);
+}
+
 void AddNewOperationHandler::handle(AppModel *model, std::string &input)
 {
     std::string date = getStrFromInput();
@@ -11,3 +18,4 @@ void AddNewOperationHandler::handle(AppModel *model, std::string &input)
     static_cast<AppModel*>(m_model)->addNewOperation(date.c_str(), deposit.c_str(), amount, category.c_str());
     updateOperationsList(date);
 }
+
