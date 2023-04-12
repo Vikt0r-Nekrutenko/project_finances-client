57953be826fb5cdfb5bff56ae3a8dbde5bcb1418
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-12 20:57:37 +0300

    286

--- views/sources/operationhandler.hpp
+++ views/sources/operationhandler.hpp
@@ -1,12 +1,19 @@
 #ifndef OPERATIONHANDLER_HPP
 #define OPERATIONHANDLER_HPP
 
+#include <string>
 class AppModel;
 
 class OperationHandler
 {
 public:
-    virtual void handle(AppModel *model) = 0;
+    virtual void handle(AppModel *model, std::string &input) = 0;
+};
+
+class AddNewOperationHandler : public OperationHandler
+{
+public:
+    void handle(AppModel *model, std::string &input) override;
 };
 
 #endif // OPERATIONHANDLER_HPP
