a97624fe5ce37c7bb4a8288addf017a767a2446e
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-13 07:49:30 +0300

    329

--- views/sources/depositlistview.cpp
+++ views/sources/depositlistview.cpp
@@ -4,7 +4,7 @@
 DepositListView::DepositListView(AppModel *model)
     : ModelViewWithInputField(model)
 {
-    mOptrionsCount = 3;
+    mOptrionsCount = 4;
 }
 
 void DepositListView::show(stf::Renderer &renderer)
@@ -16,7 +16,8 @@ void DepositListView::show(stf::Renderer &renderer)
     renderer.drawText({0, 3}, "1.Add new deposit.");
     renderer.drawText({0, 4}, "2.Delete deposit.");
     renderer.drawText({0, 5}, "3.Change balance.");
-    renderer.drawText({0, 6}, "q.Back to menu.");
+    renderer.drawText({0, 6}, "4.Select deposit.");
+    renderer.drawText({0, 7}, "q.Back to menu.");
 
     if(mOption == 1) {
         renderer.drawText({0, InputInfoY}, "Type 'name balance' or 'q' to step back:");
@@ -24,6 +25,8 @@ void DepositListView::show(stf::Renderer &renderer)
         renderer.drawText({0, InputInfoY}, "Type deposit id or 'q' to step back:");
     } else if(mOption == 3) {
         renderer.drawText({0, InputInfoY}, "Type 'id balance' or 'q' to step back:");
+    } else if(mOption == 4) {
+        renderer.drawText({0, InputInfoY}, "Type 'id' or 'q' to step back:");
     }
 
     renderer.drawText({BeginListX, BeginListY}, "Your deposits:");
@@ -60,6 +63,10 @@ void DepositListView::onEnterHandler()
         int balance = getIntFromInput();
         static_cast<AppModel*>(m_model)->changeBalance(id, balance);
         break;
+    }
+    case 4: {
+        int id = getIntFromInput() - 1;
+        // TODO: return new OperationsListView(AppModel->deposits().at(id))
     }}
 }
 
