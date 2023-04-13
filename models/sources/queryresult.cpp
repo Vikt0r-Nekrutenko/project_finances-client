a026027f5cd14686000528fba6d21fe725599109
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-13 12:31:22 +0300

    254

--- models/sources/queryresult.cpp
+++ models/sources/queryresult.cpp
@@ -14,37 +14,27 @@ AppModel::QueryResult &AppModel::QueryResult::select()
 AppModel::QueryResult &AppModel::QueryResult::filterByDeposit(const std::string &deposit)
 {
     for(QueryResult::iterator it = begin(); it != end(); )
-        if((*it)->deposit() == deposit)
-            it = erase(it);
-        else
-            ++it;
+        it = ((*it)->deposit() == deposit) ? erase(it) : ++it;
     return *this;
 }
 
 AppModel::QueryResult &AppModel::QueryResult::filterByYear(const int year)
 {
     for(QueryResult::iterator it = begin(); it != end(); )
-        if(QDateTime().fromString((*it)->date().c_str(), "yyyy-MM-dd").date().year() == year)
-            it = erase(it);
-        else
-            ++it;
+        it = (QDateTime().fromString((*it)->date().c_str(), "yyyy-MM-dd").date().year() == year) ? erase(it) : ++it;
     return *this;
 }
 
 AppModel::QueryResult &AppModel::QueryResult::filterByMonth(const int month)
 {
-    auto operations = mModel->mOperationHandler.operations();
-    for(size_t i = 0; i < operations.size(); ++i)
-        if(QDateTime().fromString(operations.at(i).date().c_str(), "yyyy-MM-dd").date().month() == month)
-            push_back(&operations.at(i));
+    for(QueryResult::iterator it = begin(); it != end(); )
+        it = (QDateTime().fromString((*it)->date().c_str(), "yyyy-MM-dd").date().month() == month) ? erase(it) : ++it;
     return *this;
 }
 
 AppModel::QueryResult &AppModel::QueryResult::filterByDay(const int day)
 {
-    auto operations = mModel->mOperationHandler.operations();
-    for(size_t i = 0; i < operations.size(); ++i)
-        if(QDateTime().fromString(operations.at(i).date().c_str(), "yyyy-MM-dd").date().day() == day)
-            push_back(&operations.at(i));
+    for(QueryResult::iterator it = begin(); it != end(); )
+        it = (QDateTime().fromString((*it)->date().c_str(), "yyyy-MM-dd").date().day() == day) ? erase(it) : ++it;
     return *this;
 }
