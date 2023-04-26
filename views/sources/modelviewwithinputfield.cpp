ae605f9862e0399549eb8c06bcd83fa04b8e6ee8
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-26 23:28:59 +0300

    418

--- views/sources/modelviewwithinputfield.cpp
+++ views/sources/modelviewwithinputfield.cpp
@@ -25,23 +25,8 @@ stf::smv::IView *ModelViewWithInputField::keyEventsHandler(const int key)
             if(tmpOption < 1 || tmpOption > mOptrionsCount)
                 return this;
             mOption = tmpOption;
-            delete mInputField;
-            mInputField = new ActiveInputField(0, InputPrewievY);
+            setInputField(new ActiveInputField(0, InputPrewievY));
         } else {
-//            if(key == 'q' && mInputField->text().empty()) {
-//                delete mInputField;
-//                mInputField = new InactiveInputField(0, InputPrewievY);
-//                mOption = 0;
-//            } else if(key == 13 || key == 10) {
-////                mInputBackup = mInput;
-//                stf::smv::IView *resultView = onEnterHandler();
-//                delete mInputField;
-//                mInputField = new InactiveInputField(0, InputPrewievY);
-//                mOption = 0;
-//                return resultView;
-//            } else {
-//                mInputField->keyEventsHandler(key);
-//            }
             return mInputField->keyEventsHandler(this, key);
         }
     } catch(const std::exception &msg) {
@@ -61,8 +46,6 @@ void ModelViewWithInputField::show(stf::Renderer &renderer)
     for(int i = 2; i < int(stf::Renderer::log.y() - 3); ++i)
         renderer.drawPixel({BeginListX - 1, i}, '|');
 
-//    if(mOption != 0)
-//        renderer.draw({0, InputPrewievY}, ">> %s", mInput.c_str());
     mInputField->show(renderer);
 }
 
