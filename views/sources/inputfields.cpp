55ef15dc2c664a71993353ba575d8574d7453704
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-26 04:21:14 +0300

    396

--- views/sources/inputfields.cpp
+++ views/sources/inputfields.cpp
@@ -18,7 +18,20 @@ ActiveInputField::ActiveInputField(int x, int y) : InputField(x, y) { }
 
 void ActiveInputField::keyEventsHandler(const int key)
 {
-    mText.insert(mCursor++, 1, key);
+    if(key == ',') {
+        if(mCursor > 0)
+            --mCursor;
+        return;
+    } else if(key == '.') {
+        if(mCursor < int(mText.length()))
+            ++mCursor;
+        return;
+    } else if((key == 127 || key == 8) && mCursor > 0) {
+        mText.erase(--mCursor, 1);
+        return;
+    } else if((key < '0' || key > 'z') && key != ' ' && key != '-' && key != '+')
+        return;
+    mText.insert(mCursor++, 1, char(key));
 }
 
 void ActiveInputField::show(stf::Renderer &renderer) const
