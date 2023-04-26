a27e99d6f361fb7a08cadd08f7206a1fe3d9bece
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-26 03:24:09 +0300

    384

--- views/headers/modelviewwithinputfield.hpp
+++ views/headers/modelviewwithinputfield.hpp
@@ -23,6 +23,7 @@ protected:
 class InactiveInputField : public InputField
 {
 public:
+    InactiveInputField(int x, int y) : InputField(x,y) { }
     void keyEventsHandler(const int) override { return; }
     void show(stf::Renderer &) override { return; }
 };
