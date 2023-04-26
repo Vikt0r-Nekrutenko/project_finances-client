443f49fc48fdc604f4eece3628618fcbbdc6de14
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-26 03:57:24 +0300

    393

--- views/sources/inputfields.cpp
+++ views/sources/inputfields.cpp
@@ -9,6 +9,11 @@ void InactiveInputField::keyEventsHandler(const int) { return; }
 
 void InactiveInputField::show(stf::Renderer &) const { return; }
 
+InputField *InactiveInputField::changeState() const
+{
+    return new ActiveInputField(X, Y);
+}
+
 ActiveInputField::ActiveInputField(int x, int y) : InputField(x, y) { }
 
 void ActiveInputField::keyEventsHandler(const int key)
@@ -20,4 +25,9 @@ void ActiveInputField::show(stf::Renderer &renderer) const
 {
     renderer.draw({X, Y}, ">> %s", mText.c_str());
     renderer.draw({3 + X + mCursor, Y}, "%CR%c", mCursor >= int(mText.length()) ? ' ' : mText.at(mCursor));
+}
+
+InputField *ActiveInputField::changeState() const
+{
+    return new InactiveInputField(X, Y);
 }
