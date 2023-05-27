69848fdee05a72a5281373f80ea2d58add39787a
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-05-27 22:01:35 +0300

    517

--- gui/sources/main.cpp
+++ gui/sources/main.cpp
@@ -3,6 +3,7 @@
 #include "headers/depositmodelhandler.hpp"
 #include "headers/categorymodelhandler.hpp"
 #include "headers/operationmodelhandler.hpp"
+#include "headers/debtmodelhandler.hpp"
 
 int main(int argc, char *argv[])
 {
@@ -10,9 +11,37 @@ int main(int argc, char *argv[])
     DepositModelHandler depo;
     CategoryModelHandler cats;
     OperationModelHandler oper;
+    DebtModelHandler debts;
 
-    auto &op = oper.operations().back();
-    std::cout << op.rawCategory(cats).name() << std::endl;
+    int option = 0;
+    do {
+        std::cout << "Choose an option:\n"
+                     "1.Deposits.\n"
+                     "2.Debts.\n"
+                     "3.Categories.\n"
+                     "0.Exit.\n"
+                     " >> ";
 
+        std::cin >> option;
+
+        switch (option)
+        {
+        case 1:
+            for(const auto &deposit : depo.deposits())
+                std::cout << deposit.name() << "\t" << deposit.balance() << std::endl;
+
+            break;
+        case 2:
+            for(const auto &debt : debts.debts())
+                std::cout << debt.name() << "\t" << debt.amount() << std::endl;
+
+            break;
+        case 3:
+            for(const auto &category : cats.categories())
+                std::cout << category.name() << "\t" << category.type() << std::endl;
+
+            break;
+        }
+    } while(option);
     return 0;//a.exec();
 }
