35ea29701cb9c1f14cedf40a7f772fa84021f2e2
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-06-09 23:28:46 +0300

    530

--- gui/sources/imenu.cpp
+++ gui/sources/imenu.cpp
@@ -3,3 +3,10 @@
 
 IMenu::IMenu(const MenuBar *mbar)
     : mMenuBar{mbar} { }
+
+void IMenu::show(stf::Renderer &renderer)
+{
+    renderer.drawLine({Width, 1}, {Width, renderer.Size.y}, '|');
+    renderer.drawPixel({0, 2 + Selector}, '[');
+    renderer.drawPixel({Width - 1, 2 + Selector}, ']');
+}
