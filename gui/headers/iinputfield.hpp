d99b013e11891cdbb6aefd57148f450056e074bb
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-06-10 14:48:39 +0300

    571

--- gui/headers/iinputfield.hpp
+++ gui/headers/iinputfield.hpp
@@ -5,6 +5,13 @@
 
 class IView;
 
+constexpr int VK_UPWARDS_ARROW = 24;
+constexpr int VK_DOWNWARDS_ARROW = 25;
+constexpr int VK_RIGHT_ARROW = 26;
+constexpr int VK_LEFT_ARROW = 27;
+constexpr int VK_BACKSPACE1 = 127;
+constexpr int VK_BACKSPACE2 = 8;
+
 class IInputField
 {
 public:
@@ -40,6 +47,22 @@ public:
     {
         renderer.draw({x, y}, ">> %s", Text.c_str());
         renderer.draw({3 + x + mCursor, y}, "%CR%c", mCursor >= int(Text.length()) ? ' ' : Text.at(mCursor));
+    }
+
+    IView *keyHandler(IView *sender, int key) override
+    {
+        if(key == VK_LEFT_ARROW) {
+            if(mCursor > 0)
+                --mCursor;
+        } else if(key == VK_RIGHT_ARROW) {
+            if(mCursor < int(Text.length()))
+                ++mCursor;
+        } else if((key == VK_BACKSPACE1 || key == VK_BACKSPACE2) && mCursor > 0) {
+            Text.erase(--mCursor, 1);
+        } else if((key > '0' && key < 'z') || key == ' ' || key == '-' || key == '+') {
+            Text.insert(mCursor++, 1, char(key));
+        }
+        return sender;
     }
 };
 
