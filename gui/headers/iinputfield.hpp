d4c3dbfe3437974087e8eca8956c0f25e59d52e1
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-06-12 00:29:25 +0300

    578

--- gui/headers/iinputfield.hpp
+++ gui/headers/iinputfield.hpp
@@ -19,8 +19,28 @@ public:
     std::string Text;
 
     virtual ~IInputField() = default;
-    virtual void show(stf::Renderer &renderer, int x, int y) = 0;
-    virtual IView *keyHandler(IView *sender, int key) = 0;
+
+    virtual void show(stf::Renderer &renderer, int x, int y)
+    {
+        renderer.draw({x, y}, ">> %s", Text.c_str());
+        renderer.draw({3 + x + mCursor, y}, "%CR%c", mCursor >= int(Text.length()) ? ' ' : Text.at(mCursor));
+    }
+
+    virtual IView *keyHandler(IView *sender, int key)
+    {
+        if(key == VK_LEFT_ARROW) {
+            if(mCursor > 0)
+                --mCursor;
+        } else if(key == VK_RIGHT_ARROW) {
+            if(mCursor < int(Text.length()))
+                ++mCursor;
+        } else if((key == VK_BACKSPACE1 || key == VK_BACKSPACE2) && mCursor > 0) {
+            Text.erase(--mCursor, 1);
+        } else if((key >= '0' && key <= 'z') || key == ' ' || key == '-' || key == '+') {
+            Text.insert(mCursor++, 1, char(key));
+        }
+        return sender;
+    }
 
     std::string getStr()
     {
