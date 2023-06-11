840b558f4d2bebb8a9f101c0322b35d6c9d65eab
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-06-12 01:04:26 +0300

    588

--- gui/sources/imenu.cpp
+++ gui/sources/imenu.cpp
@@ -50,7 +50,7 @@ void ActiveMenu::show(stf::Renderer &renderer)
 void ActiveMenu::recalculateBarWidth()
 {
     for(size_t i = 0; i < mMenuBar->size(); ++i) {
-        if(mMenuBar->at(i)->caption().length() > Width)
+        if(mMenuBar->at(i)->caption().length() > size_t(Width))
             Width = mMenuBar->at(i)->caption().length() + 4;
     }
 }
