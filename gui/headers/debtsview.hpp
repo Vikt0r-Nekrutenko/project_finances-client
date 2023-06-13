8a7129c9059c1921b37a92fce040962d3346250d
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-06-13 22:30:31 +0300

    629

--- gui/headers/debtsview.hpp
+++ gui/headers/debtsview.hpp
@@ -14,4 +14,59 @@ public:
     IView *keyHandler(int key) override;
 };
 
+namespace input_views {
+namespace debts_views {
+
+class IDebtView : public DebtsView, public IViewWithInputField
+{
+public:
+
+    IDebtView(AppModel *model, ViewHolder *holder, const std::string &inputTitle);
+
+    void show(stf::Renderer &renderer) override;
+
+    IView *keyHandler(int key) override;
+
+protected:
+
+    const std::string mInputTitle;
+};
+
+class AddNewDebtView : public IDebtView
+{
+public:
+
+    AddNewDebtView(AppModel *model, ViewHolder *holder);
+
+    IView *onEnterPressHandler() override;
+};
+
+class ChangeNameView : public IDebtView
+{
+public:
+
+    ChangeNameView(AppModel *model, ViewHolder *holder);
+
+    IView *onEnterPressHandler() override;
+};
+
+class ChangeAmountView : public IDebtView
+{
+public:
+
+    ChangeAmountView(AppModel *model, ViewHolder *holder);
+
+    IView *onEnterPressHandler() override;
+};
+
+class DeleteDebtView : public IDebtView
+{
+public:
+
+    DeleteDebtView(AppModel *model, ViewHolder *holder);
+
+    IView *onEnterPressHandler() override;
+};
+
+}}
 #endif // DEBTSVIEW_HPP
