f69d78c0f2b770946ba63100212bde830ee6be3a
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-06-14 01:05:38 +0300

    636

--- gui/headers/appmodel.hpp
+++ gui/headers/appmodel.hpp
@@ -4,6 +4,7 @@
 #include "headers/depositmodelhandler.hpp"
 #include "headers/debtmodelhandler.hpp"
 #include "headers/categorymodelhandler.hpp"
+#include "headers/operationmodelhandler.hpp"
 
 class AppModel
 {
@@ -12,6 +13,26 @@ public:
     DepositModelHandler Deposits;
     DebtModelHandler Debts;
     CategoryModelHandler Categories;
+
+    DepositModel *selectedDeposit()
+    {
+        return mSelectedDeposit;
+    }
+
+    const OperationModelHandler &operations() const
+    {
+        return mOperations;
+    }
+
+    void selectDeposit(size_t id)
+    {
+        mSelectedDeposit = &Deposits.deposits().at(id);
+    }
+
+private:
+
+    OperationModelHandler mOperations;
+    DepositModel *mSelectedDeposit = nullptr;
 };
 
 #endif // APPMODEL_HPP
