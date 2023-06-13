77001d0421d0f72430c6ccef579349e3a84dfb6d
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-06-13 22:12:15 +0300

    624

--- gui/sources/debtsview.cpp
+++ gui/sources/debtsview.cpp
@@ -8,3 +8,18 @@ DebtsView::DebtsView(AppModel *model, ViewHolder *holder)
                                             });
     mActiveMenuBar->recalculateBarWidth();
 }
+
+void DebtsView::show(stf::Renderer &renderer)
+{
+    IView::show(renderer);
+    mMenuBar->show(renderer);
+    drawLogItem(renderer, mMenuBar->Width);
+
+    int index = 1;
+}
+
+IView *DebtsView::keyHandler(int key)
+{
+    switchMenuBar(key);
+    return mMenuBar->keyHandler(this, key);
+}
