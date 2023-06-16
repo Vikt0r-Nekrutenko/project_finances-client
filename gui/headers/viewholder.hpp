7e797c002c199ab9433a29a5d9834eb596b102c8
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-06-17 01:30:00 +0300

    650

--- gui/headers/viewholder.hpp
+++ gui/headers/viewholder.hpp
@@ -1,69 +1,24 @@
 #ifndef VIEWHOLDER_HPP
 #define VIEWHOLDER_HPP
 
-#include "mainview.hpp"
-#include "depositsview.hpp"
-#include "debtsview.hpp"
-#include "categoriesview.hpp"
-#include "operationsview.hpp"
-#include "appmodel.hpp"
+#include "renderer.hpp"
+#include <list>
+
+class IView;
 
 class ViewHolder
 {
 public:
 
-    ViewHolder(AppModel *model);
     ~ViewHolder();
-    
-    IView *getStartView();
-    IView *getCloseView();
-
-    IView *getDepositView();
-    IView *getAddNewDepositView();
-    IView *getChangeBalanceView();
-    IView *getDeleteDepositView();
-    IView *getSelectDepositView();
-
-    IView *getDebtsView();
-    IView *getAddNewDebtView();
-    IView *getChangeAmountView();
-    IView *getDeleteDebtView();
-
-    IView *getCategoriesView();
-    IView *getAddNewCategoryView();
-    IView *getDeleteCategoryView();
-
-    IView *getOperationsView();
-    IView *getAddNewOperationView();
-    IView *getDeleteOperationView();
-    IView *getSelectOperationsView();
+    void operator = (IView *view);
+    void show(stf::Renderer &renderer);
+    IView *keyHandler(int key);
 
 private:
 
-    AppModel *mModel;
-
-    MainView mMainView;
-    CloseView mCloseView;
-
-    DepositsView mDepositView;
-    input_views::deposits_views::AddNewDepositView mAddNewDepositView;
-    input_views::deposits_views::ChangeBalanceView mChangeBalanceView;
-    input_views::deposits_views::DeleteDepositView mDeleteDepositView;
-    input_views::deposits_views::SelectDepositView mSelectDepositView;
-
-    DebtsView mDebtsView;
-    input_views::debts_views::AddNewDebtView mAddNewDebtView;
-    input_views::debts_views::ChangeAmountView mChangeAmountView;
-    input_views::debts_views::DeleteDebtView mDeleteDebtView;
-
-    CategoriesView mCategoriesView;
-    input_views::categories_views::AddNewCategoryView mAddNewCategoryView;
-    input_views::categories_views::DeleteCategoryView mDeleteCategoryView;
-
-    OperationsView *mOperationsView = nullptr;
-    input_views::operations_views::AddNewOperationView *mAddNewOperationView = nullptr;
-    input_views::operations_views::DeleteOperationView *mDeleteOperationView = nullptr;
-    input_views::operations_views::SelectOperationsView *mSelectOperationsView = nullptr;
+    std::list<IView *> mViews;
+    IView *mCurrentView;
 };
 
 #endif // VIEWHOLDER_HPP
--- gui/sources/viewholder.cpp
+++ gui/sources/viewholder.cpp
@@ -1,126 +1,26 @@
 #include "viewholder.hpp"
+#include "iview.hpp"
 
-ViewHolder::ViewHolder(AppModel *model)
-    : mModel{model},
-    mMainView{model, this},
-    mCloseView{model, this},
-    mDepositView{model, this},
-    mAddNewDepositView{model, this},
-    mChangeBalanceView{model, this},
-    mDeleteDepositView{model, this},
-    mSelectDepositView{model, this},
-    mDebtsView{model, this},
-    mAddNewDebtView{model, this},
-    mChangeAmountView{model, this},
-    mDeleteDebtView{model, this},
-    mCategoriesView{model, this},
-    mAddNewCategoryView{model, this},
-    mDeleteCategoryView{model, this}
-{}
 
 ViewHolder::~ViewHolder()
 {
-    delete mOperationsView;
-    delete mAddNewOperationView;
-    delete mDeleteOperationView;
-    delete mSelectOperationsView;
+    for(auto view : mViews)
+        delete view;
 }
 
-IView *ViewHolder::getStartView()
+void ViewHolder::operator =(IView *view)
 {
-    return &mMainView;
-}
+    for(auto it = mViews.begin(); it != mViews.end(); ++it) {
+        if(*it == view)
+            return;
 
-IView *ViewHolder::getDepositView()
-{
-    return &mDepositView;
-}
+        if(typeid(**it).hash_code() == typeid(*view).hash_code()) {
+            delete *it;
+            *it = mCurrentView = view;
+            return;
+        }
 
-IView *ViewHolder::getCloseView()
-{
-    return &mCloseView;
-}
-
-IView *ViewHolder::getAddNewDepositView()
-{
-    return &mAddNewDepositView;
-}
-
-IView *ViewHolder::getChangeBalanceView()
-{
-    return &mChangeBalanceView;
-}
-
-IView *ViewHolder::getDeleteDepositView()
-{
-    return &mDeleteDepositView;
-}
-
-IView *ViewHolder::getSelectDepositView()
-{
-    return &mSelectDepositView;
-}
-
-IView *ViewHolder::getDebtsView()
-{
-    return &mDebtsView;
-}
-
-IView *ViewHolder::getAddNewDebtView()
-{
-    return &mAddNewDebtView;
-}
-
-IView *ViewHolder::getChangeAmountView()
-{
-    return &mChangeAmountView;
-}
-
-IView *ViewHolder::getDeleteDebtView()
-{
-    return &mDeleteDebtView;
-}
-
-IView *ViewHolder::getCategoriesView()
-{
-    return &mCategoriesView;
-}
-
-IView *ViewHolder::getAddNewCategoryView()
-{
-    return &mAddNewCategoryView;
-}
-
-IView *ViewHolder::getDeleteCategoryView()
-{
-    return &mDeleteCategoryView;
-}
-
-IView *ViewHolder::getOperationsView()
-{
-    if(mOperationsView == nullptr) {
-        mOperationsView = new OperationsView(mModel, this);
+        mViews.push_back(view);
     }
-    return mOperationsView->recalculateList();
 }
 
-IView *ViewHolder::getAddNewOperationView()
-{
-    if(mAddNewOperationView == nullptr)
-        mAddNewOperationView = new input_views::operations_views::AddNewOperationView(mModel, this);
-    return mAddNewOperationView->recalculateList();
-}
-
-IView *ViewHolder::getDeleteOperationView()
-{
-    if(mDeleteOperationView == nullptr)
-        mDeleteOperationView = new input_views::operations_views::DeleteOperationView(mModel, this);
-    return mDeleteOperationView->recalculateList();
-}
-
-IView *ViewHolder::getSelectOperationsView()
-{
-    if(mSelectOperationsView == nullptr)
-        mSelectOperationsView = new input_views::operations_views::SelectOperationsView(mModel, this);
-    return mSelectOperationsView->recalculateList();
-}
