b29298fbdbbec1a293bcf12c4840a0e08ccefe6d
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-06-25 19:56:09 +0300

    718

--- gui/sources/mainview.cpp
+++ gui/sources/mainview.cpp
@@ -37,9 +37,6 @@ void MainView::show(stf::Renderer &renderer)
     drawInfo("Earn:......", mModel->totalEarn());
     drawInfo("Deposits:..", mModel->totalDeposits());
     drawInfo("Debts:.....", mModel->totalDebts());
-//    renderer.draw({mMenuBar->Width + 1, y++}, "Earn:......%m.00 %s", int(float(mModel->totalEarn()) / mModel->currentCurrency().second), mModel->currentCurrency().first.c_str());
-//    renderer.draw({mMenuBar->Width + 1, y++}, "Deposits:..%m.00 UAH", mModel->totalDeposits());
-//    renderer.draw({mMenuBar->Width + 1, y++}, "Debts:.....%m.00 UAH", mModel->totalDebts());
 
     ++y;
     renderer.drawText({mMenuBar->Width + 1, y++}, "P&Ls:");
@@ -47,10 +44,6 @@ void MainView::show(stf::Renderer &renderer)
     drawColoredInfo("Monthly..", mModel->monthlyPnL());
     drawColoredInfo("Year.....", mModel->yearPnL());
     drawColoredInfo("Total....", mModel->totalPnL());
-//    renderer.draw({mMenuBar->Width + 1, y++}, (mModel->todayPnL()   > 0 ? "Today....%CG%m.00 UAH%CD" : "Today....%CR%m.00 UAH%CD"), mModel->todayPnL());
-//    renderer.draw({mMenuBar->Width + 1, y++}, (mModel->monthlyPnL() > 0 ? "Monthly..%CG%m.00 UAH%CD" : "Monthly..%CR%m.00 UAH%CD"), mModel->monthlyPnL());
-//    renderer.draw({mMenuBar->Width + 1, y++}, (mModel->yearPnL()    > 0 ? "Year.....%CG%m.00 UAH%CD" : "Year.....%CR%m.00 UAH%CD"), mModel->yearPnL());
-//    renderer.draw({mMenuBar->Width + 1, y++}, (mModel->totalPnL()   > 0 ? "Total....%CG%m.00 UAH%CD" : "Total....%CR%m.00 UAH%CD"), mModel->totalPnL());
 
     ++y;
     renderer.drawText({mMenuBar->Width + 1, y++}, "Favorite categories(by current month):");
@@ -58,15 +51,12 @@ void MainView::show(stf::Renderer &renderer)
         renderer.drawLine({mMenuBar->Width + 1, y}, {mMenuBar->Width + 15, y}, '.');
         renderer.draw({mMenuBar->Width + 1, y}, "%s:", category.first.c_str());
         drawColoredInfo("", category.second, 15);
-//        renderer.draw({mMenuBar->Width + 15, y++}, (category.second > 0 ? "%CG%m.00 UAH%CD" : "%CR%m.00 UAH%CD"), category.second);
     }
 
     ++y;
     renderer.drawText({mMenuBar->Width + 1, y++}, "Min & Max loss(by current month):");
     drawColoredInfo((mModel->minMaxLoss().first.first+": "), mModel->minMaxLoss().first.second);
     drawColoredInfo((mModel->minMaxLoss().second.first+": "), mModel->minMaxLoss().second.second);
-//    renderer.draw({mMenuBar->Width + 1, y++}, (mModel->minMaxLoss().first.second > 0 ? "%s: %CG%m.00 UAH%CD" : "%s: %CR%m.00 UAH%CD"), mModel->minMaxLoss().first.first.c_str(), mModel->minMaxLoss().first.second);
-//    renderer.draw({mMenuBar->Width + 1, y++}, (mModel->minMaxLoss().second.second > 0 ? "%s: %CG%m.00 UAH%CD" : "%s: %CR%m.00 UAH%CD"), mModel->minMaxLoss().second.first.c_str(), mModel->minMaxLoss().second.second);
 }
 
 IView *MainView::keyHandler(int key)
