659edbb0fb1aa1824dd0bdea7d43b512bf6ae8f3
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-06-26 03:49:27 +0300

    721

--- gui/sources/main.cpp
+++ gui/sources/main.cpp
@@ -6,17 +6,36 @@
 #include "mainview.hpp"
 #include "appmodel.hpp"
 
+class StartView : public IView
+{
+public:
+
+    StartView(AppModel *model)
+        : IView(model) { }
+
+    void show(stf::Renderer &renderer) override
+    {
+        renderer.drawText({renderer.Size.x / 2, renderer.Size.y / 2}, "Start view");
+    }
+
+    IView *keyHandler(int) override
+    {
+        return this;
+    }
+};
+
 class App : public stf::Window
 {
 public:
 
     AppModel model;
     MainView *mainView = new MainView{&model};
+    StartView *startView = new StartView(&model);
     ViewHolder viewHolder;
 
     App()
     {
-        viewHolder = mainView;
+        viewHolder = startView;
     }
 
     bool onUpdate(const float) override
