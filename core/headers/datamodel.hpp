5d53a549847e89459e3a3641f9460f1a8ec91056
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-12-19 01:05:53 +0200

    746

--- core/headers/datamodel.hpp
+++ core/headers/datamodel.hpp
@@ -22,12 +22,14 @@ protected:
 
     virtual QNetworkReply *sendCRUDRequest(const std::string &additionalPath, const QJsonObject &data, const std::string &request, const QUrlQuery &params = {});
     RemoteStatus replyHandler(QNetworkReply *reply, const std::string &noErrorMsg) const;
+    inline int version() { return mVersion; }
 
 private:
 
     static std::string
         AuthValue,
         MainPath;
+    int mVersion {0};
 };
 
 #endif // DATAMODEL_HPP
