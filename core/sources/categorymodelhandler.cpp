9dfac9bcb6d52422a256a860724f12f68cde9b44
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-12-19 19:02:26 +0200

    773

--- core/sources/categorymodelhandler.cpp
+++ core/sources/categorymodelhandler.cpp
@@ -62,16 +62,6 @@ void CategoryModelHandler::parseJsonArray(const QJsonArray &replyJsonArray)
     }
 }
 
-const std::vector<CategoryModel> &CategoryModelHandler::categories() const
-{
-    return mCategories;
-}
-
-std::vector<CategoryModel> &CategoryModelHandler::categories()
-{
-    return mCategories;
-}
-
 std::vector<CategoryModel>::iterator CategoryModelHandler::findByName(const std::string &name)
 {
     return std::find_if(mCategories.begin(), mCategories.end(), [&](const CategoryModel &model){
