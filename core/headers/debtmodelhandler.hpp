b04f739a6f5fa3df2ec8157c6b107ace91c97259
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-12-23 20:36:19 +0200

    849

--- core/headers/debtmodelhandler.hpp
+++ core/headers/debtmodelhandler.hpp
@@ -7,16 +7,30 @@
 class CORE_EXPORT DebtModelHandler : public DataModelHandler
 {
 public:
+
+    class CORE_EXPORT Query : public std::vector<DebtModel *>
+    {
+    public:
+
+        Query(DebtModelHandler *handler);
+        const Query &select();
+        int sum() const;
+        std::vector<DebtModel *>::const_iterator findByName(const std::string &name) const;
+
+    private:
+
+        DebtModelHandler *mHandler { nullptr };
+
+    } query {this};
+
     DebtModelHandler();
     ~DebtModelHandler() override;
 
     void addNewDebt(const std::string &name, int amount);
     void updateDebt(int index, const std::string &name, int amount);
     void deleteDebt(int index);
-
-    inline const std::vector<DebtModel> &debts() const { return mDebts; }
-
-    std::vector<DebtModel>::iterator findByName(const std::string &name);
+    void increaseAmount(int index, int amount);
+    void decreaseAmount(int index, int amount);
 
 protected:
 
