b7b8c69c64f7cfea16b605d77b80d14934edcf69
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2024-03-08 23:21:05 +0200

    986

--- gui/sources/icommand.cpp
+++ gui/sources/icommand.cpp
@@ -1,5 +1,23 @@
 #include "icommand.hpp"
+#include <iostream>
 
 std::string ICommand::info() const { return ""; }
 
 std::string ICommand::help() const { return ""; }
+
+std::string commands::Help::info() const
+{
+    return "Commands list:";
+}
+
+std::string commands::Help::help() const
+{
+    return "Get a commands list with descriptions";
+}
+
+void commands::Help::execute()
+{
+    for(const auto &item : *mCommandsList) {
+        std::cout << "\t" << item.first << " - " << item.second->help() << std::endl;
+    }
+}
